715f7c11ffd3f2fc70abf5b00fa713a6f6cdab51
Ksiaoking <102038338+Ksiaoking@users.noreply.github.com>
2025-09-22 11:10:32 +0300

    Create follow.py

---
+++ follow.py
@@ -0,0 +1,31 @@
+# tiktok-accounts-create
+TikTok-Account-Creator:  - create accounts unlimited  - no shadowban  - uploader / follow / like / comment / report bot  - captcha solver  - device registration and activation  - all algorithms updated (gorgon, khronos, landon, argus mssdk...)  - api versions: api16, api22 (both available)  - a
+
+
+### tiktok API
+### login to the user by email and username
+### register new device
+### receive information of the user through user_id
+### receive information about your authorized profile
+### get list of user's subscribers
+### get list of user's subscriptions
+### receive user posts (publications)
+### receive comments for posts
+### like comments
+### like posts
+### subscribe to user
+### search
+### search hashtags
+### search post hashtags
+### search users
+### download Post By Video Id
+### download tiktok video without watermark
+### follow user
+### unfollow user
+### create account
+### resolve captcha
+### create accounts
+
+## I will provide proof upon request.
+
+Telegram: https://t.me/ksiaoking
