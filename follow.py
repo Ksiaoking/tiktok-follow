d68cfb99750c1f9353012ef6af773be5d46d8383
Ksiaoking <102038338+Ksiaoking@users.noreply.github.com>
2025-09-29 14:58:23 +0300

    Update follow.py

--- follow.py
+++ follow.py
@@ -2,6 +2,7 @@
 
 TikTok-Account-Creator:  - create accounts unlimited  - no shadowban  - uploader / follow / like / comment / report bot  - captcha solver  - device registration and activation  - all algorithms updated (gorgon, khronos, landon, argus mssdk...)  - api versions: api16, api22 (both available)  - a
 
+
 ### tiktok API
 ### login to the user by email and username
 ### register new device
